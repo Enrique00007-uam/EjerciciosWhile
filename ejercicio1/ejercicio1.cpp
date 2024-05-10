16d4bd520b743e87b1153560b0c3d9ccc3d1f906
Enrique00007-uam <emsolis@uamv.edu.ni>
2024-05-09 22:26:09 -0600

    cambie algunas cosas

--- ejercicio1/ejercicio1.cpp
+++ ejercicio1/ejercicio1.cpp
@@ -1,3 +1,9 @@
+/*1. Dado un número natural n se desea calcular la suma de los números
+naturales desde 1 hasta n. Codifica el programa que resuelva este
+planteamiento*/
+
+
+
 #include <iostream>
 
 using namespace std;
@@ -7,7 +13,7 @@ int suma(int num);
 int main(int argc, char const *argv[])
 {
     int num = 1;
-    cout << "Ingrese el número del que desea calcular la suma: "  << endl;
+    cout << "Ingrese el numero del que desea calcular la suma: "  << endl;
     cin >> num;
     suma(num);
     return 0;
@@ -20,5 +26,5 @@ int suma(int num) {
         suma = suma + i;
         i++;
     }
-    cout << "La suma del 1 hasta el número dado es: " << suma;
+    cout << "La suma del 1 hasta el numero dado es: " << suma;
 }
